7c44b9cff7adc1ba6a5351960a1402d05f825381
luiselv <luiselv@outlook.com>
2022-08-09 08:23:30 -0500

    code refactor

---
+++ Sat.Recruitment.DAL/UserDAO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sat.Recruitment.Entities;
+using Sat.Recruitment.Interfaces;
+
+namespace Sat.Recruitment.DAL
+{
+    public class UserDAO
+    {
+        #region Instance
+        private static UserDAO objInstance = null;
+        private UserDAO()
+        {
+        }
+        public static UserDAO Instance
+        {
+            get
+            {
+                if (objInstance == null)
+                    objInstance = new UserDAO();
+
+                return objInstance;
+            }
+        }
+        #endregion
+
+        IUserDAO implementer;
+        public void SetImplementer(IUserDAO userDao)
+        {
+            implementer = userDao;
+        }
+
+        public bool Create(User user)
+        {
+            return implementer.Create(user);
+        }
+
+        public List<User> GetAll()
+        {
+            return implementer.GetAll();
+        }
+    }
+}
